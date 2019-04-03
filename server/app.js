019519fbb8f18a26f23d4e340fe8e590d0cd1d5f
Mirza Selim Reza <selimrezael@gmail.com>
2019-04-03 08:47:10 +0600

    first commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+
+}));
+
+app.listen(4000, () => {
+  console.log('now listening to port 4000');
+});
