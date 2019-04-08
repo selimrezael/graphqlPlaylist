2d23aa45e21b29156c2ace017c1c2f8cda31edcf
Mirza Selim Reza <selimrezael@gmail.com>
2019-04-08 08:52:00 +0600

    connecting to mongodb atlas

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://reza:147147@cluster0-lfp4w.mongodb.net/test?retryWrites=true')
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
